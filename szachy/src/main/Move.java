a19ea3a635da81af85e08af9c9a51cd6bba21b71
barbulek <106350334+plewinho@users.noreply.github.com>
2023-05-14 10:36:48 +0200

    Update Move.java

--- szachy/src/main/Move.java
+++ szachy/src/main/Move.java
@@ -4,14 +4,15 @@ import pieces.Piece;
 
 public class Move {
 
-    int oldCol;
-    int oldRow;
-    int newCol;
-    int newRow;
+int oldCol; // kolumna, z której ruszono figurą
+int oldRow; // rząd, z którego ruszono figurą
+int newCol; // kolumna, na którą przesunięto figurę
+int newRow; // rząd, na który przesunięto figurę
 
-    Piece piece;
-    Piece capture;
+    Piece piece; // przesuwana figura 
+    Piece capture; //ewentualna zbita figura
 
+    //konstruktor klasy move 
     public Move(Board board, Piece piece, int newCol, int newRow){
 
         this.oldCol = piece.col;
@@ -20,6 +21,8 @@ public class Move {
         this.newRow = newRow;
 
         this.piece = piece;
+        
+        //zapisnaie zbita figura, jezeli na nowych kordach jest figura przeciwnika
         this.capture = board.getPiece(newCol, newRow);
 
 
